4dbd3c84698ec2b9ea24ec159439208cd812e684
FranZoia6 <39228851+FranZoia6@users.noreply.github.com>
2022-08-22 19:15:07 -0300

    tabla paises

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,18 +12,22 @@ export class PorPaisComponent {
 
   termino:string = "";
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
     
   buscar(){
     this.hayError = false;
     console.log(this.termino);
-    this.paisService.buscarPais(this.termino).subscribe((resp)=>{
-      console.log(resp);
+    this.paisService.buscarPais(this.termino).subscribe((paises)=>{
+      console.log(paises);
+      this.paises = paises;
+  
       
     },(err)=>{
       console.log('Error');
       this.hayError = true;
+      this.paises = [];
     });
   }
 
