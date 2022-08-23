e2d45c173ecd63ce870de2d7698b5d94a110635d
FranZoia6 <39228851+FranZoia6@users.noreply.github.com>
2022-08-23 11:34:05 -0300

    componet input

---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent  {
+
+  @Output()onEnter: EventEmitter<string> = new EventEmitter();
+
+  termino: string ='';
+  
+  buscar(){
+    this.onEnter.emit(this.termino);
+
+  }
+
+  constructor() { }
+
+
+}
